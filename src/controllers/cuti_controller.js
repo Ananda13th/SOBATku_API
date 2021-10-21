eb46465fc965fc973c64c4c37d12142644b7dda2
Ananda13th <pramonoananda@gmail.com>
2021-10-21 13:17:24 +0700

    Tambah Fungsi Cuti

---
+++ src/controllers/cuti_controller.js
@@ -0,0 +1,60 @@
+const { json } = require("body-parser");
+const Cuti = require("../models/cuti");
+const moment = require('moment');
+
+exports.cariJadwalCuti = function async(req, res) {
+    Cuti.cekCuti(req.params.kodeJadwal, function(error, result) {
+        if(error)
+            res.send(error);
+        if(result.length == 0) {
+            res.json({error_code: 200, message: "Success", data : false})
+        }
+        else {
+            res.json({error_code: 200, message: "Success", data : true})
+        }
+    })
+}
+
+exports.buatJadwalCuti = function async(req, res) {
+    var request = req.body;
+    var startDate = moment(request.mulai);
+    var endDate = moment(request.selesai);
+
+    Cuti.ambilJadwal(request.kode_dokter, function(error, result) {
+        if(error)
+            res.send(error);
+        if(result.length == 0 )
+            res.json({error_code: 400, message: "Jadwal Tidak Ditemukan"});
+        else {
+            result.forEach(jadwal => {
+                var start = startDate.clone()
+                if(req.params.hari != null) {
+                    while(start.isSameOrBefore(endDate)) {
+                        if(req.params.hari == jadwal.hari && req.params.hari == start.weekday()) {
+                            if(req.params.jam != null && req.params.jam == jadwal.jam.substring(0,2)) {
+                                var kodeJadwal = request.kode_dokter + "." + start.format('YYMMDD') + jadwal.jam.substring(0,2);
+                                Cuti.inputCuti(request.kode_dokter, kodeJadwal, 
+                                    function(error, result) {}
+                                );
+                            }
+                        }
+                        start.add(1, 'days');
+                    }
+                } 
+                else {
+                    console.log(req.params.hari != null)
+                    while(start.isSameOrBefore(endDate)) {
+                        if(jadwal.hari == start.weekday()) {
+                            var kodeJadwal = request.kode_dokter + "." + start.format('YYMMDD') + jadwal.jam.substring(0,2);
+                            Cuti.inputCuti(request.kode_dokter, kodeJadwal, 
+                                function(error, result) {}
+                            );
+                        }
+                        start.add(1, 'days');
+                    }
+                }
+            });
+            res.json({error_code: 200, message: "Berhasil Menambah Cuti, Harap Cek di Daftar Cuti"});
+        }
+    });
+}
---
+++ src/models/cuti.js
@@ -0,0 +1,55 @@
+const express = require('express');
+var dbConn = require('./../../config/db.config');
+
+var Cuti = function(cuti) {
+    this.kode_dokter  = cuti.kode_dokter;
+    this.mulai        = cuti.mulai;
+    this.selesai      = cuti.selesai;
+}
+
+
+Cuti.cekCuti = function(kodeJadwal, result) {
+    dbConn.query("SELECT * FROM jadwal_cuti WHERE kode_jadwal = ?", kodeJadwal,
+        function(err, res) {
+            if(err) {
+                console.log("error: ", err);
+                result(err, null);
+            } else {
+                result(null, res);
+            }
+        }
+    )
+}
+
+Cuti.ambilJadwal = function(kodeDokter, result) {
+    dbConn.query(
+        "SELECT jadwal.hari, jadwal.jam FROM jadwal_dokter" +
+        " JOIN dokter ON dokter.kode_dokter = jadwal_dokter.kode_dokter" +
+        " JOIN jadwal ON jadwal.id_jadwal = jadwal_dokter.id_jadwal" +
+        " WHERE dokter.kode_dokter = ?", kodeDokter, 
+        function(err, res) {
+            if(err) {
+                console.log("error: ", err);
+                result(err, null);
+            } else {
+                result(null, res);
+            }
+        }
+    );
+}
+
+Cuti.inputCuti = function(kodeDokter, kodeJadwal, result) {
+    dbConn.query(
+        "INSERT INTO jadwal_cuti SET kode_dokter=?, kode_jadwal=?", [kodeDokter,kodeJadwal], 
+        function(err, res) {
+            if(err) {
+                console.log("error: ", err);
+                result(err, null);
+            } else {
+                result(null, res);
+            }
+        }
+    )
+}
+
+module.exports = Cuti;
