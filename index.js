6277c26a3a5d507479a79d08ccdf3ae970c83eb3
Ananda13th <pramonoananda@gmail.com>
2021-11-15 13:33:59 +0700

    Tambah Route

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var cors = require('cors')
-
-const port = process.env.PORT || 6000;
+const cors = require('cors')
+const {json} = require('body-parser');
+const port = process.env.PORT || 3001;
 const app = express();
 
 app.listen(port, () => {
@@ -11,29 +11,29 @@ app.listen(port, () => {
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
 app.use(cors());
 
-var middleware = function (req, res, next) {
+const middleware = function (req, res, next) {
     const dateNow =  Math.floor(new Date().getTime() / 1000);
     var max5 = dateNow + 300;
     var min5 = dateNow - 300;
-    var id = req.headers.id;
+    var id = "ancient one";
     var time = req.headers.time;
     var token = req.headers.token;
     if(time >= min5 && time <= max5) {
         var secretKey = "secretkey";
-        var signature = require('crypto').createHmac("sha256", secretKey)
-        .update(id + "&" + time).digest('base64');
+        var signature = require('crypto').createHmac("sha256", secretKey).update(id + "&" + time).digest('base64');
         if(token == signature)
             next()
         else 
             res.json({error_code: 401, message: "Token Salah"})
     }
     else
-    res.json({error_code: 401, message: "Unidentified User"});
+        res.json({error_code: 401, message: "Unidentified User"});
 }
 
-//app.use(middleware);
+app.use(middleware);
 
 const pasienRoutes = require('./src/routes/pasien_route');
 const userRoutes = require('./src/routes/user_route');
@@ -49,6 +49,9 @@ const verificationRoutes = require('./src/routes/verification_routes');
 const favoritRoutes = require('./src/routes/favorit_routes');
 const cutiRoutes = require('./src/routes/cuti_route');
 const aktivasiRoutes = require('./src/routes/aktivasi_route');
+const bpjsRoutes = require('./src/routes/cek_bpjs_route');
+const jadwalRoutes = require('./src/routes/jadwal_route');
+const medinRoutes = require('./src/routes/medin_route');
 
 app.use('/api/v1/pasien', pasienRoutes);
 app.use('/api/v1/user', userRoutes);
@@ -64,6 +67,9 @@ app.use('/api/v1/verifikasi', verificationRoutes);
 app.use('/api/v1/favorit', favoritRoutes);
 app.use('/api/v1/cuti', cutiRoutes);
 app.use('/api/v1/aktivasi', aktivasiRoutes);
+app.use('/api/v1/bpjs', bpjsRoutes);
+app.use('/api/v1/jadwal_jam', jadwalRoutes);
+app.use('/', medinRoutes);
 
 
 
