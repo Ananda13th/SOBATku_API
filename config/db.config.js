595e581c132f5cfa3aa240fc2f68093b52d1676a
Ananda13th <pramonoananda@gmail.com>
2021-12-15 13:49:23 +0700

    Ubah Ke Pool Connection

--- config/db.config.js
+++ config/db.config.js
@@ -1,24 +1,91 @@
-const mysql = require('mysql');
+var mysql = require('mysql');
 
-/* MOCK */
-// const dbConn = mysql.createConnection({
-//     host        : 'localhost',
-//     user        : 'root',
-//     password    : '',
+var dbConn = mysql.createPool({
+    host            : 'localhost',
+    user            : 'mydev',
+    password        : 'KunciKata1945+',
+    database        : 'sobatku'
+}); 
+
+/* METHOD 2 */
+
+
+// var db_config = {
+//   host        : 'localhost',
+//     user        : 'mydev',
+//     password    : 'KunciKata1945+',
 //     database    : 'sobatku'
+// };
+
+// //- Create the connection variable
+// var dbConn = mysql.createConnection(db_config);
+
+// //- Establish a new connection
+// dbConn.connect(function(err){
+//   if(err) {
+//       // mysqlErrorHandling(connection, err);
+//       console.log("\n\t *** Cannot establish a connection with the database. ***");
+
+//       dbConn = reconnect(dbConn);
+//   }else {
+//       console.log("\n\t *** New connection established with the database. ***")
+//   }
 // });
 
-/* LIVE */
-const dbConn = mysql.createConnection({
-    host        : 'localhost',
-    user        : 'mydev',
-    password    : 'KunciKata1945+',
-    database    : 'sobatku'
-});
+// //- Reconnection function
+// function reconnect(dbConn){
+//   console.log("\n New connection tentative...");
 
-dbConn.connect(function(err) {
-    if(err) throw err;
-    console.log("Database Connected!");
-})
+//   //- Destroy the current connection variable
+//   if(dbConn) dbConn.destroy();
+
+//   //- Create a new one
+//   var dbConn = mysql.createConnection(db_config);
+
+//   //- Try to reconnect
+//   dbConn.connect(function(err){
+//       if(err) {
+//           //- Try to connect every 2 seconds.
+//           setTimeout(reconnect, 2000);
+//       }else {
+//           console.log("\n\t *** New connection established with the database. ***")
+//           return dbConn;
+//       }
+//   });
+// }
+
+// //- Error listener
+// dbConn.on('error', function(err) {
+
+//   //- The server close the connection.
+//   if(err.code === "PROTOCOL_CONNECTION_LOST"){    
+//       console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+//       dbConn = reconnect(dbConn);
+//   }
+
+//   //- Connection in closing
+//   else if(err.code === "PROTOCOL_ENQUEUE_AFTER_QUIT"){
+//       console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+//       dbConn = reconnect(dbConn);
+//   }
+
+//   //- Fatal error : connection variable must be recreated
+//   else if(err.code === "PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR"){
+//       console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+//       dbConn = reconnect(dbConn);
+//   }
+
+//   //- Error because a connection is already being established
+//   else if(err.code === "PROTOCOL_ENQUEUE_HANDSHAKE_TWICE"){
+//       console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+//   }
+
+//   //- Anything else
+//   else{
+//       console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+//       dbConn = reconnect(dbConn);
+//   }
+
+// });
 
 module.exports = dbConn;
