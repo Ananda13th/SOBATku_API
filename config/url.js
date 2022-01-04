798f4bfafd36d5fe1ac9c9c95a7ed32702611291
Ananda13th <pramonoananda@gmail.com>
2022-01-04 14:00:49 +0700

    File Semua URL

---
+++ config/url.js
@@ -0,0 +1,4 @@
+const pendaftaran_url = "http://112.109.19.210:9696/pendaftaran/public/index.php/antrian";
+const bpjs_url        = "https://apijkn-dev.bpjs-kesehatan.go.id/vclaim-rest-dev/"
+
+module.exports = {pendaftaran_url, bpjs_url}
